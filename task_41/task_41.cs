8b744a9612ad24502b21179014e4d9c64d4f7a4a
Alena Savostyanova <el.savostyanova90@yandex.ru>
2023-02-14 15:30:43 +0300

    Homework. Seminar 6.

---
+++ task_41/task_41.cs
@@ -0,0 +1,26 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 3
+
+System.Console.WriteLine("Enter numbers: ");
+var arr = Array.ConvertAll(Console.ReadLine()!.Split(), int.Parse);
+
+int CountPosNum(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (arr[i] > 0)
+        {
+            count++;
+        }
+
+    }
+    return count;
+}
+
+int res = CountPosNum(arr);
+System.Console.WriteLine(res);
---
+++ task_43/task_43.cs
@@ -0,0 +1,27 @@
+﻿// Задача 43. Напишите программу, которая найдёт точку пересечения 
+// двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+
+double Numbers(string text)
+{
+    System.Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+
+}
+
+double k1 = Numbers("Введите число k1: ");
+double b1 = Numbers("Введите число b1: ");
+double k2 = Numbers("Введите число k2: ");
+double b2 = Numbers("Введите число b2: ");
+
+
+void Coordinate(double a, double b, double c, double d, out double x, out double y)
+{
+    x = (d - b) / (a - c);
+    y = a * x + b;
+}
+
+Coordinate(k1, b1, k2, b2, out double x, out double y);
+System.Console.WriteLine($"({x}, {y})");
+
+
